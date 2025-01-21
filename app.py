c78c1b17f3b48a77c433134c2d11c3c7e699d945
beurrekk <suphannika.kk@gmail.com>
2025-01-21 09:32:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ st.set_page_config(layout="wide")
 uploaded_file = "review_new_new.csv"
 df = pd.read_csv(uploaded_file, encoding='ISO-8859-1')
 
-
 # Preprocess data
 df['Review Date'] = pd.to_datetime(df['Review Date'], format="%d/%m/%Y")
 df['Month'] = df['Review Date'].dt.strftime('%B')
